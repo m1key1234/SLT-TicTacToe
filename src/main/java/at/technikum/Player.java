17369134f8669f928ec0f7ddce017b4c2b0aa25f
unknown <wi24b087@technikum-wien.at>
2025-06-29 22:26:17 +0200

    US-001: Make Move

---
+++ src/main/java/at/technikum/Player.java
@@ -0,0 +1,14 @@
+package at.technikum;
+
+public class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
+
